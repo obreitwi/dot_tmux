1e0e1dd6356f814bdb3565f0ed7ffae6a55e4934
Oliver Breitwieser <obreitwi@pio2pc145.pio.uni-heidelberg.de>
2015-09-22 17:34:06 +0200

    Updated install script

--- install.sh
+++ install.sh
@@ -4,5 +4,5 @@ SCRIPTDIR=$(cd $(dirname $0); pwd)
 
 echo $SCRIPTDIR
 
-ln -s -f -v $SCRIPTDIR/scripts/toggle-mouse-mode.sh ~/usr/bin/tmux-toggle-mouse-mode
+ln -s -f -v $SCRIPTDIR/scripts/toggle-mouse-mode.sh ~/.local/bin/tmux-toggle-mouse-mode
 
