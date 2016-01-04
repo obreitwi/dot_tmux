cad37ac9defcdec2d53e936a554c53305d0a240b
Oliver Breitwieser <obreitwi@kip.uni-heidelberg.de>
2016-01-04 14:32:29 +0100

    Updated config to new mouse mode

--- scripts/toggle-mouse-mode.sh
+++ scripts/toggle-mouse-mode.sh
@@ -1,7 +1,15 @@
 #!/bin/zsh
 
 # g: global, w: window-options, v: only show value
-toggle=$(tmux show-option -gwv mode-mouse)
+toggle=$(tmux show-option -gwv mode-mouse 2>&1)
+style="old"
+
+echo $toggle | grep -q "^unknown option:"
+
+if [[ $? == 0 ]]; then
+	toggle=$(tmux show-option -gv mouse)
+	style="new"
+fi
 
 if [[ $toggle == "off" ]]; then
 	toggle="on"
@@ -10,10 +18,14 @@ else
 	toggle="off"
 fi
 
-tmux display-message "Toggling mouse mode $toggle"
 
-tmux set -gq mouse-select-pane $toggle
-tmux set -gq mouse-resize-pane $toggle
-tmux set -gq mouse-select-window $toggle
-tmux set -gq mode-mouse $toggle
+tmux display-message "Toggling mouse mode $toggle (using $style style)"
 
+if [[ $style == "old" ]]; then
+	tmux set -gq mouse-select-pane $toggle
+	tmux set -gq mouse-resize-pane $toggle
+	tmux set -gq mouse-select-window $toggle
+	tmux set -gq mode-mouse $toggle
+else
+	tmux set -gq mouse $toggle
+fi
